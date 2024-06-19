b9cf8c9b4de4effe78c6ca6a442f0da19c5dba4d
Dante Castro Garro <profe.paiche@gmail.com>
2024-06-19 21:09:27 +0400

    New version

--- colors/chalkboard.lua
+++
@@ -1,7 +0,0 @@
-package.loaded['chalkboard'] = nil
-package.loaded['chalkboard.colors'] = nil
-package.loaded['chalkboard.named_colors'] = nil
-package.loaded['chalkboard.theme'] = nil
-package.loaded['chalkboard.util'] = nil
-
-require('chalkboard').setup()
---
+++ colors/toytiza.lua
@@ -0,0 +1,7 @@
+package.loaded['toytiza'] = nil
+-- package.loaded['toytiza.colors'] = nil
+package.loaded['toytiza.named_colors'] = nil
+package.loaded['toytiza.theme'] = nil
+package.loaded['toytiza.util'] = nil
+
+require('toytiza').setup()
